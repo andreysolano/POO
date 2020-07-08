fc395ce21b645c143ab510fb367a18620a1e8af2
Lionxcode <bduartea@unal.edu.co>
2020-07-08 01:30:39 -0500

    capas y filtros

--- ProyectoFinal/PaintPoseNet/PaintPoseNet.js
+++ ProyectoFinal/PaintPoseNet/PaintPoseNet.js
@@ -14,7 +14,13 @@ let eye1Y = 0;
 let r_Wrist = new p5.Vector();
 let l_Wrist = new p5.Vector();
 
-let canvas; 
+let canvas0;  //******
+let canvas1;
+let canvas2;
+let canvas3;
+
+let canvasUsed = 0;
+
 let pointer; 
 
 let pincelR = 0;
@@ -37,8 +43,15 @@ function setup() {
   poseNet = ml5.poseNet(video, modelReady);
   poseNet.on('pose', gotPoses);
 
-  canvas = createGraphics(640, 480);
-  canvas.background(255, 255, 255);
+  canvas0 = createGraphics(640, 480); //**
+  canvas1 = createGraphics(640, 480);
+  canvas2 = createGraphics(640, 480);
+  canvas3 = createGraphics(640, 480);
+  
+  canvas0.background(255); //**
+  canvas1.background(100);
+  canvas2.background(0); 
+  canvas3.background(180);
 
   pointer = loadImage('data/pointer.png');
   rect(0, 0, 100, 100);
@@ -49,6 +62,13 @@ function setup() {
   boton4= new Boton(570,240,40,40,0,255,0);
   botonImg1 = new BotonImg(570, 300, 40, 40, 255, 255, 255, imgBorr);
   guardar= new Guarda(570, 360, 40, 40, 255, 255, 255, imgGuar, c);
+  botonCanvas0 = new BotonCanvas(500,60,40,40,245,64,64,0);
+  botonCanvas1 = new BotonCanvas(500,120,40,40,245,239,49,1);
+  botonCanvas2 = new BotonCanvas(500,180,40,40,50,100,200,2);
+  botonCanvas3 = new BotonCanvas(500,240,40,40,0,255,0,3);
+  botonFiltro0 = new BotonFiltro(450,60,40,40,245,64,64);
+  botonFiltro1 = new BotonFiltro(450,120,40,40,245,239,49);
+  botonFiltro2 = new BotonFiltro(450,180,40,40,50,100,200);
 }
 
 function modelReady() {
@@ -98,14 +118,40 @@ function draw() {
 
   
   let d = dist(nose.x, nose.y, eye1X, eye1Y);
-  canvas.noStroke();
-  canvas.fill(pincelR, pincelG, pincelB);        //<-------****
-  canvas.ellipse(r_Wrist.x, r_Wrist.y, d);
   
-  
-  image(canvas, 0, 0);
+
+  ////**** condicionales para las capas ****////
+  if(canvasUsed == 0){
+    canvas0.noStroke();
+    canvas0.fill(pincelR, pincelG, pincelB);        
+    canvas0.ellipse(r_Wrist.x, r_Wrist.y, d);
+    image(canvas0, 0, 0);
+  }else{
+    if(canvasUsed == 1){
+      canvas1.noStroke();
+      canvas1.fill(pincelR, pincelG, pincelB);        
+      canvas1.ellipse(r_Wrist.x, r_Wrist.y, d);
+      image(canvas1, 0, 0);
+    }else{
+      if(canvasUsed == 2){
+        canvas2.noStroke();
+        canvas2.fill(pincelR, pincelG, pincelB);        
+        canvas2.ellipse(r_Wrist.x, r_Wrist.y, d);
+        image(canvas2, 0, 0);
+      }else{
+        if(canvasUsed == 3){
+          canvas3.noStroke();
+          canvas3.fill(pincelR, pincelG, pincelB);        
+          canvas3.ellipse(r_Wrist.x, r_Wrist.y, d);
+          image(canvas3, 0, 0);
+        }
+      }
+    }
+  }
 
   image(pointer, l_Wrist.x, l_Wrist.y, d, d );  
+  
+  
   
   
   boton1.show();
@@ -121,6 +167,20 @@ function draw() {
   guardar.show();
   guardar.showImg();
   guardar.saver();
+  botonCanvas0.showN();
+  botonCanvas1.showN();
+  botonCanvas2.showN();
+  botonCanvas3.showN();
+  botonCanvas0.setCanvas();
+  botonCanvas1.setCanvas();
+  botonCanvas2.setCanvas();
+  botonCanvas3.setCanvas();
+  botonFiltro0.showF();
+  botonFiltro1.showF();
+  botonFiltro2.showF();
+  botonFiltro0.setFiltro();
+  botonFiltro1.setFiltro();
+  botonFiltro2.setFiltro();
 }
 
 
